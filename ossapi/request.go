dab5ba6957732237c22c00a5bb7c9016c9e46d6a
C@Z <cz.theng@gmail.com>
2015-10-22 14:40:14 +0800

    Add error Log

--- ossapi/request.go
+++ ossapi/request.go
@@ -28,8 +28,9 @@ type Request struct {
 
 func (req *Request) Send() (rsp *Response, err error) {
 	URL := "http://" + path.Join(req.Host, req.Path)
-	req.httpReq, err = http.NewRequest("GET", URL, nil)
+	req.httpReq, err = http.NewRequest(req.Method, URL, nil)
 	if err != nil {
+		Logger.Error("http.NewRequest(req.Method,URL, nil) Error:%s", err.Error())
 		return
 	}
 	req.httpReq.ProtoMinor = 1
@@ -38,12 +39,14 @@ func (req *Request) Send() (rsp *Response, err error) {
 	//req.httpreq.Header.Add("Host", req.Host)
 	auth, err := req.Auth()
 	if err != nil {
+		Logger.Error("req.Auth() Error:%s", err.Error)
 		return
 	}
 	req.httpReq.Header.Add("Authorization", auth)
 	//fmt.Println("Req head:", req.httpreq.Header)
 	httprsp, err := httpClient.Do(req.httpReq)
 	if err != nil {
+		Logger.Error("httpClient.Do(req.httpReq) Error:%s", err.Error())
 		return
 	}
 	rsp = &Response{httpRsp: httprsp}
@@ -57,10 +60,12 @@ func (req *Request) Send() (rsp *Response, err error) {
 		body := make([]byte, cntLen*10)
 		_, err = httprsp.Body.Read(body)
 		if err != nil && err != io.EOF {
+			Logger.Error("httprsp.Body.Read(body) Error:%s", err.Error())
 			return
 		}
 		err = xml.Unmarshal(body, rstErr)
 		if err != nil {
+			Logger.Error("xml.Unmarshal(body, rstErr) Error:%s", err.Error())
 			return
 		}
 		rstErr.ErrDetailMsg = string(body)
@@ -79,6 +84,7 @@ func (req *Request) Auth() (authStr string, err error) {
 	authStr = "OSS " + accessKeyID + ":"
 	sigStr, err := req.Signature()
 	if err != nil {
+		Logger.Error("sigStr, err := req.Signature() Error:%s", err.Error())
 		return
 	}
 	authStr += sigStr
@@ -99,6 +105,7 @@ func (req *Request) Signature() (sig string, err error) {
 	sigStr += ossHeaders + resources
 	sig, err = Base64AndHmacSha1([]byte(accessKeySecret), []byte(sigStr))
 	if err != nil {
+		Logger.Error("sig, err = Base64AndHmacSha1([]byte(accessKeySecret), []byte(sigStr)) Error:%s", err.Error())
 		return
 	}
 	return
