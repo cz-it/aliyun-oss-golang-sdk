05446ca4f192f345f6b76873fb5e90132d920830
C@Z <cz.theng@gmail.com>
2015-10-23 19:40:32 +0800

    Add GetBucketWebsite & GetBucketReferer

---
+++ ossapi/get_bucket_website.go
@@ -0,0 +1,65 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package ossapi
+
+import (
+	"encoding/xml"
+	"path"
+	"strconv"
+)
+
+/*
+// Redefine in put_bucket_website
+type IndexInfo struct {
+	Suffix string
+}
+type ErrorInfo struct {
+	Key string
+}
+type WebsiteInfo struct {
+	XMLName       xml.Name  `xml:"WebsiteConfiguration"`
+	IndexDocument IndexInfo `xml:"IndexDocument"`
+	ErrorDocument KeyInfo   `xml:"ErrorDocument"`
+}
+*/
+
+func GetBucketWebsite(name, location string) (info *WebsiteInfo, ossapiError *Error) {
+	host := name + "." + location + ".aliyuncs.com"
+	resource := path.Join("/", name) + "/"
+	req := &Request{
+		Host:     host,
+		Path:     "/?website",
+		Method:   "GET",
+		Resource: resource,
+		SubRes:   []string{"website"}}
+	rsp, err := req.Send()
+	if err != nil {
+		if _, ok := err.(*Error); !ok {
+			Logger.Error("GetService's Send Error:%s", err.Error())
+			ossapiError = OSSAPIError
+			return
+		}
+	}
+	if rsp.Result != ESUCC {
+		ossapiError = err.(*Error)
+		return
+	}
+	bodyLen, err := strconv.Atoi(rsp.httpRsp.Header["Content-Length"][0])
+	if err != nil {
+		Logger.Error("GetService's Send Error:%s", err.Error())
+		ossapiError = OSSAPIError
+		return
+	}
+	body := make([]byte, bodyLen)
+	rsp.httpRsp.Body.Read(body)
+	info = new(WebsiteInfo)
+	err = xml.Unmarshal(body, info)
+	if err != nil {
+		Logger.Error("GetService's Send Error:%s", err.Error())
+		ossapiError = OSSAPIError
+		return
+	}
+	return
+}
