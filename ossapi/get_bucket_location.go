293a5be93bc28ef70c91475ce35d67f7ebb34ff2
C@Z <cz.theng@gmail.com>
2015-10-23 18:31:20 +0800

    Add GetBucketLocation

---
+++ ossapi/get_bucket_location.go
@@ -0,0 +1,56 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package ossapi
+
+import (
+	"encoding/xml"
+	"path"
+	"strconv"
+)
+
+type LocationInfo struct {
+	XMLName  xml.Name `xml:"LocationConstraint"`
+	Location string   `xml:",chardata"`
+}
+
+func GetBucketLocation(name string) (location string, ossapiError *Error) {
+	host := name + ".oss.aliyuncs.com"
+	resource := path.Join("/", name) + "/"
+	req := &Request{
+		Host:     host,
+		Path:     "/?location",
+		Method:   "GET",
+		Resource: resource,
+		SubRes:   []string{"location"}}
+	rsp, err := req.Send()
+	if err != nil {
+		if _, ok := err.(*Error); !ok {
+			Logger.Error("GetService's Send Error:%s", err.Error())
+			ossapiError = OSSAPIError
+			return
+		}
+	}
+	if rsp.Result != ESUCC {
+		ossapiError = err.(*Error)
+		return
+	}
+	bodyLen, err := strconv.Atoi(rsp.httpRsp.Header["Content-Length"][0])
+	if err != nil {
+		Logger.Error("GetService's Send Error:%s", err.Error())
+		ossapiError = OSSAPIError
+		return
+	}
+	body := make([]byte, bodyLen)
+	rsp.httpRsp.Body.Read(body)
+	locationInfo := new(LocationInfo)
+	err = xml.Unmarshal(body, locationInfo)
+	if err != nil {
+		Logger.Error("GetService's Send Error:%s", err.Error())
+		ossapiError = OSSAPIError
+		return
+	}
+	location = locationInfo.Location
+	return
+}
---
+++ ossapi/get_bucket_location_test.go
@@ -0,0 +1,21 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+package ossapi
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetBucketLocation(t *testing.T) {
+	if nil != Init("v8P430U3UcILP6KA", "EB9v8yL2aM07YOgtO1BdfrXtdxa4A1") {
+		t.Fail()
+	}
+	if info, err := GetBucketLocation("test-put-bucket2"); err != nil {
+		fmt.Println(err.ErrNo, err.HttpStatus, err.ErrMsg, err.ErrDetailMsg)
+	} else {
+		t.Log("Get BucketLocation Success")
+		fmt.Println(info)
+	}
+}
