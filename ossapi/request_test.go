d943453f691a099858de4f48840fc6488c6d78a1
C@Z <cz.theng@gmail.com>
2015-10-27 01:44:59 +0800

    Cover Rate for CI

--- ossapi/request_test.go
+++ ossapi/request_test.go
@@ -14,8 +14,27 @@ func TestError(t *testing.T) {
 }
 
 func TestDo(t *testing.T) {
-	req := &Request{Host: "oss.aliyuncs.com", Path: "/", Method: "GET", Resource: "/"}
+	headers := map[string]string{"Content-XX": "adf"}
+	req := &Request{
+		Host:      "oss.aliyuncs.com",
+		Path:      "/",
+		ExtHeader: headers,
+		SubRes:    []string{"aa"},
+		Body:      []byte("aaa"),
+		CntType:   "text/html",
+		Method:    "GET",
+		Resource:  "/"}
+	req.AddXOSS("oss-xx", "abc")
 	rsp, err := req.Send()
+
+	fmt.Println("====Method Error=======")
+	req.Method = "error"
+	rsp, err = req.Send()
+
+	fmt.Println("====URL Error=======")
+	req.Host = "nimeidenimei"
+	rsp, err = req.Send()
+
 	if err != nil {
 		fmt.Println(err)
 	}
