bea7d76ebfe02a304a7d73135ddb2bef4d846154
C@Z <cz.theng@gmail.com>
2015-10-27 02:31:00 +0800

    Cover Rate for CI

--- ossapi/log/log_test.go
+++ ossapi/log/log_test.go
@@ -3,3 +3,24 @@
  */
 
 package log
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	l, err := NewFileLogger(".ossapilog", "ossapi")
+	if err != nil {
+		fmt.Errorf("Create Logger Error\n")
+		return
+	}
+	l.SetMaxFileSize(1024 * 1024 * 100) //100MB
+	l.SetLevel(LDEBUG)
+	l.Error("Error")
+	l.Debug("Debug:int a is %d", 10)
+	l.Fatal("fatal")
+	l.Info("Info")
+	l.SetCallDepth(2)
+	l.Warning("Warning")
+}
