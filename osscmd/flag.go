a981bb7a614c7f4b70d5847e7b48f2ba41aacc96
C@Z <cz.theng@gmail.com>
2015-10-29 02:52:34 +0800

    Add flag

---
+++ osscmd/flag.go
@@ -0,0 +1,91 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/cz-it/aliyun-oss-golang-sdk/ossapi"
+	"os"
+)
+
+var Flag FlagInfo
+
+const (
+	helpStr = `
+	osscmd is a tool for ossapi.
+
+	Usage:
+
+		osscmd command [arguments]
+
+		The commands are:
+
+		    init : init osscmd's evniroment 
+	`
+	initHelpStr = `
+	Usage: init -i id -s secort 
+	    init osscmd with Access Key ID id and Access Key Secort secort
+	Commands:
+		-i id : Access Key ID
+		-s secort : Access Key Secort
+		-h/--hlep : Show Help
+
+	`
+)
+
+type FlagInfo struct {
+	Version bool
+	ID      string
+	Secort  string
+}
+
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("OSS API's Command Tool\n")
+		fmt.Println(helpStr)
+	}
+
+	flag.BoolVar(&Flag.Version, "v", false, "Show OSS API Command Tool's Version")
+	flag.StringVar(&Flag.ID, "i", "", " Access Key ID")
+	flag.StringVar(&Flag.Secort, "s", "", " Access Key Secort")
+}
+
+func Usage() {
+	fmt.Println(helpStr)
+	os.Exit(0)
+}
+
+func InitUsage() {
+	fmt.Println(initHelpStr)
+	os.Exit(0)
+}
+
+func parseArgs() {
+	var err error
+	if "-h" == os.Args[1] || "help" == os.Args[1] {
+		Usage()
+	} else if "-v" == os.Args[1] || "version" == os.Args[1] {
+		fmt.Println("Cur Version:", ossapi.Version())
+		return
+	} else if "init" == os.Args[1] {
+		if "-h" == os.Args[2] || "--help" == os.Args[2] {
+			InitUsage()
+		}
+		flag.CommandLine.Parse(os.Args[2:])
+		if Flag.ID == "" || Flag.Secort == "" {
+			flag.Usage()
+			return
+		}
+		if err = writeCfg(Flag.ID, Flag.Secort); err != nil {
+			fmt.Println("[FAIL]: Init Error ", err.Error())
+		} else {
+			fmt.Println("[SUCC]: Init Success!")
+		}
+		return
+	} else {
+		Usage()
+	}
+}
--- osscmd/main.go
+++ osscmd/main.go
@@ -5,9 +5,59 @@
 package main
 
 import (
-	"fmt"
+	"encoding/xml"
+	"os"
 )
 
+const (
+	configPath = "~/.osscmd/config.xml"
+)
+
+type ConfigInfo struct {
+	XMLName         xml.Name `xml:"Config"`
+	AccessKeyID     string   `xml:"AccessKeyID"`
+	AccessKeySecret string   `xml:"AccessKeySecret"`
+}
+
+var (
+	accessKeyID     string
+	accessKeySecret string
+)
+
+func readCfg() (err error) {
+	cfg := new(ConfigInfo)
+	cfgData := make([]byte, 10240)
+	fd, err := os.Open(configPath)
+	_, err = fd.Read(cfgData)
+	if err != nil {
+		return
+	}
+	err = xml.Unmarshal(cfgData, cfg)
+	if err != nil {
+		return
+	}
+	accessKeyID = cfg.AccessKeyID
+	accessKeySecret = cfg.AccessKeySecret
+	return
+}
+
+func writeCfg(ID, Secret string) (err error) {
+	fd, err := os.Create(configPath)
+	if err != nil {
+		return
+	}
+	cfg := &ConfigInfo{AccessKeyID: ID, AccessKeySecret: Secret}
+	cfgData, err := xml.Marshal(cfg)
+	if err != nil {
+		return
+	}
+	_, err = fd.Write(cfgData)
+	if err != nil {
+		return
+	}
+	return
+}
+
 func main() {
-	fmt.Println("OSS Command...")
+	parseArgs()
 }
